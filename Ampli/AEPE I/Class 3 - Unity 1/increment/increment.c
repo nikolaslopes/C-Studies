0e44a0432d3c4846a997b4463d805f670f448da2
nikolaslopes <nikolaslopes.dev@gmail.com>
2021-12-28 19:12:39 -0300

    :sparkles: increment operation

---
+++ Ampli/AEPE I/Class 3 - Unity 1/increment/increment.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int x = 0;
+  int y = 0;
+  
+  int incremented_before_execution = ++x;
+  int incremented_after_execution = y++;
+  
+  printf("\n X Value incremented before the first execution: X = %d", incremented_before_execution);
+
+  printf("\n");
+
+  printf("\n Y Value: Y = %d", incremented_after_execution);
+  printf("\n Y value incremented after the first execution: Y = %d", y);
+}
