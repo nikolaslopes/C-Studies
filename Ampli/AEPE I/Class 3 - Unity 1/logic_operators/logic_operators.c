c39c130d54899e659dae6395f086cc037664d1d5
nikolaslopes <nikolaslopes.dev@gmail.com>
2021-12-29 01:13:55 -0300

    :fire: add logic operators

--- Ampli/AEPE I/Class 3 - Unity 1/logic_operators/logic_operators.c
+++ Ampli/AEPE I/Class 3 - Unity 1/logic_operators/logic_operators.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-  int x = 0;
-  int y = 0;
-
-  printf("\n Enter the X value: ");
-  scanf("%d", &x);
-
-  printf("\n Enter de Y value: ");
-  scanf("%d", &y);
-
-  system("CLS");
+int main() {
+  int x = 5, y = 10, z = 5;
 
   printf("\n 1 = true\n 0 = false\n");
+  printf("\n\n ((%d == %d) && (%d == %d)) = %d", x, y, x, z, ((x == y) && (x == z)));
+  printf("\n\n ((%d == %d) || (%d == %d)) = %d", x, y, x, z, ((x == y) || (x == z)));
+  printf("\n\n !((%d == %d) || (%d == %d)) = %d", x, y, x, z, !((x == y) || (x == z)));
 
-  printf("\n %d and %d are the same number? %d", x, y, x == y);
-  printf("\n %d is greater than %d? %d", x, y, x > y);
-  printf("\n %d is less than %d? %d", x, y, x < y);
-  printf("\n %d is less or equal to %d? %d\n\n\n", x, y, x <= y);
-
+  printf("\n\n\n");
   system("PAUSE");
   system("CLS");
 }
