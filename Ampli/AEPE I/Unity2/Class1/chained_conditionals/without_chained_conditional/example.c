35caff5f41468dd5b410f662ffe8f6f08392daaa
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-01-01 23:36:38 -0400

    :fire: students grade verification without chained conditional

---
+++ Ampli/AEPE I/Unity2/Class1/chained_conditionals/without_chained_conditional/example.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  float student_grade = 0;
+
+  printf("\n Enter the student's final grade: ");
+  scanf("%f", &student_grade);
+
+  if (student_grade >= 60) {
+    printf("\n The student is approved");
+  } else if (student_grade >= 50) {
+    printf("\n The student is in recovery");
+  } else {
+    printf("\n The student is disapproving");
+  }
+
+  printf("\n\n\n");
+  system("PAUSE");
+  system("CLS");
+
+  return 0;
+}
