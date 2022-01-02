b452ed08298f7e0839ce16c6c3549a79dfd0c1d4
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-01-02 00:06:29 -0400

    :bug: salario descontos ex

---
+++ Ampli/AEPE I/Unity2/Class1/salario_descontos/example.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  float salario_bruto = 0, deconto_inss = 0, desconto_ir = 0, salario_liquido = 0;
+
+  system("PAUSE");
+  system("CLS");
+
+  printf("\n\n ***** Cálculo de Salário Líquido com descontos na folha de pagamento *****\n");
+  printf("\n Digite o valor do seu salário: ");
+  scanf("%f", &salario_bruto);
+
+  printf("\n\n\n");
+  system("PAUSE");
+  system("CLS");
+  return 0;
+}
