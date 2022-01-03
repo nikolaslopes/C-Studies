6fad555620ef0c01c05d5d3bda5cc197fba9503f
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-01-02 20:49:31 -0400

    :fire: add while example

---
+++ Ampli/AEPE I/Unity2/Class2/while/example.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  system("PAUSE");
+  system("CLS");
+
+  int counter = 0;
+
+  while (counter < 10) {
+    printf("\n %d - I LIKE C", counter);
+
+    counter++;
+  }
+
+  printf("\n\n\n");
+  system("PAUSE");
+  system("CLS");
+
+  return 0;
+}
