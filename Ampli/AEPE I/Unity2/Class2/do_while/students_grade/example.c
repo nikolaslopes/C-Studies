2caf4d560f2fa34318796410daca30ce6887b052
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-01-03 23:34:05 -0400

    :fire: :poop: :zap:

---
+++ Ampli/AEPE I/Unity2/Class2/do_while/students_grade/example.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+  float grade = 0;
+
+  system("PAUSE");
+  system("CLS");
+
+  do {
+    printf("\n Enter the student's final grade (min: 0, max: 10): ");
+    scanf("%f", &grade);
+    system("CLS");
+  } while (grade < 0 || grade > 10);
+
+  printf("\n Student's final grade: %.2f", grade);
+
+  printf("\n\n\n");
+  system("PAUSE");
+  system("CLS");
+
+  return 0;
+}
