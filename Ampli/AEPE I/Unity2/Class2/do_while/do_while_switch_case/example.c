54dc33560de1cde4914e0c3f2903db17033a2257
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-01-04 23:31:33 -0400

    :poop: :zap:

--- Ampli/AEPE I/Unity2/Class2/do_while/do_while_switch_case/example.c
+++ Ampli/AEPE I/Unity2/Class2/do_while/do_while_switch_case/example.c
@@ -5,9 +5,18 @@ int main() {
   float soma = 0;
   float valor;
   int option;
+  
+  system("CLS");
 
   do {
     printf("\n M E N U   D E   O P Ç Õ E S");
+    printf("\n (1) Depositar");
+    printf("\n (2) Saque");
+    printf("\n (3) Saldo");
+    printf("\n (4) Sair");
+
+    printf("\n\n Informe uma opção: ");
+    scanf("%f", &option);
   } while (option != 4);
 
   system("CLS");
