f92641b70608624bb8d1ae834eb716f03b6245c0
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-01-05 00:06:22 -0400

    :fire: finish do while and switch case

--- Ampli/AEPE I/Unity2/Class2/do_while/do_while_switch_case/example.c
+++ Ampli/AEPE I/Unity2/Class2/do_while/do_while_switch_case/example.c
@@ -2,25 +2,77 @@
 #include <stdlib.h>
 
 int main() {
-  float soma = 0;
+  float saldo = 0;
   float valor;
   int option;
   
   system("CLS");
 
   do {
-    printf("\n M E N U   D E   O P Ç Õ E S");
+    printf("\n ***** M E N U   D E   O P Ç Õ E S *****\n\n");
     printf("\n (1) Depositar");
     printf("\n (2) Saque");
     printf("\n (3) Saldo");
     printf("\n (4) Sair");
 
     printf("\n\n Informe uma opção: ");
-    scanf("%f", &option);
+    scanf("%d", &option);
+
+    switch(option) {
+      case 1:
+        system("CLS");
+        printf("\n\n * Depositar *\n");
+        printf("\n Informe o valor: ");
+        scanf("%f", &valor);
+        saldo += valor;
+
+        printf("\n\n\n");
+        system("PAUSE");
+        system("CLS");
+        break;
+      case 2:
+        system("CLS");
+        printf("\n\n * Saque *\n");
+        printf("\n Informe o valor: ");
+        scanf("%f", &valor);
+
+        if (saldo < valor) {
+          printf("\n O valor do saque é menor que o seu saldo!");
+        } else {
+          saldo -= valor;
+        }
+
+        printf("\n\n\n");
+        system("PAUSE");
+        system("CLS");
+        break;
+      case 3:
+        system("CLS");
+        printf("\n\n * Saldo *\n");
+        printf("\n Seu saldo: %.2f", saldo);
+
+        printf("\n\n\n");
+        system("PAUSE");
+        system("CLS");
+        break;
+      case 4:
+        system("CLS");
+        printf("\n\n * Aperte qualquer tecla para sair *\n");
+
+        printf("\n\n\n");
+        system("PAUSE");
+        system("CLS");
+        break;
+      default:
+        printf("\n\n * Opção inválida *");
+
+        printf("\n\n\n");
+        system("PAUSE");
+        system("CLS");
+    } 
   } while (option != 4);
 
-  system("CLS");
-
+  printf("\n Obrigado por usar nosso sistema!");
 
   printf("\n\n\n");
   system("PAUSE");
