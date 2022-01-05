a8a9d8820cc7203dfcc3d7b815c36ad7e2acb1b9
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-01-04 23:28:40 -0400

    :fire: do while with switch case

---
+++ Ampli/AEPE I/Unity2/Class2/do_while/do_while_switch_case/example.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  float soma = 0;
+  float valor;
+  int option;
+
+  do {
+    printf("\n M E N U   D E   O P Ç Õ E S");
+  } while (option != 4);
+
+  system("CLS");
+
+
+  printf("\n\n\n");
+  system("PAUSE");
+  system("CLS");
+
+  return 0;
+}
