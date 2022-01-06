29c78e2abf4b69a5400f762ee842f7e24ec64b06
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-01-06 01:13:49 -0400

    :fire: number counter with for

---
+++ Ampli/AEPE I/Unity2/Class3/for/number_counter/example.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main() {
+  int x, y;
+
+  system("CLS");
+
+  printf("\n ***** NUMBER COUNTER *****");
+  
+  printf("\n\n\n");
+  system("PAUSE");
+  system("CLS");
+
+  for (y = 0; y <=10; y++) {
+    printf("\n y = %d", y);
+  }
+
+  printf("\n\n\n");
+  system("PAUSE");
+  system("CLS");
+
+  return 0;
+}
