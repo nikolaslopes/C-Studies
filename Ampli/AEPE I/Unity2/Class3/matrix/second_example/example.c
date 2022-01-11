6fe69644c6313dd1db80be94041638cad66e1e1f
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-01-11 00:26:25 -0400

    :zap:

---
+++ Ampli/AEPE I/Unity2/Class3/matrix/second_example/example.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  system("CLS");
+
+  int linha, coluna;
+  int matriz[3][3];
+
+  int matriz_diagonal = 1;
+
+  for (linha = 0; linha < 3; )
+
+  printf("\n\n\n");
+  system("PAUSE");
+  system("CLS");
+  return 0;
+}
