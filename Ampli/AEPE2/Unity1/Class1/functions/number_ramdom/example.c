582d6752309816e7a6ec072ec0905c981ef7e89d
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-01-13 00:31:44 -0400

    :fire:

---
+++ Ampli/AEPE2/Unity1/Class1/functions/number_ramdom/example.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int* gerarRandomico() {
+  static int r[10];
+  int a;
+
+  for (a = 0; a < 10; a++) {
+    r[a] = rand() % 100;
+    printf("\n r[%d] = %d", a, r[a]);
+  }
+  return r;
+}
+
+int main() {
+
+  int *p;
+  int i;
+
+  p = gerarRandomico();
+
+  for (i = 0; i < 10; i++) {
+    printf("\n p[%d] = ")
+  }
+}
