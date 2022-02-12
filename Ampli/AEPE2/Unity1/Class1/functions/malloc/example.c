2a707520f5cd2519e41608fc251d3f50f31f966f
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-02-12 10:32:37 -0400

    :fire: adjusted some functions in C tree

--- Ampli/AEPE2/Unity1/Class1/functions/malloc/example.c
+++ Ampli/AEPE2/Unity1/Class1/functions/malloc/example.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int alocar() {
+int* alocar() {
   int *memoria = (int*) malloc(sizeof(int));
   return memoria;
 }
+
 int main() {
   int *num = alocar();
   if (num != NULL) {
     printf("\n Informe um número inteiro: ");
-    scanf("%d", num);
-    printf("\n Número informado: %d", *num);
+    scanf("%d", &num);
+    printf("\n Número informado: %d", num);
   }
 
   return 0;
